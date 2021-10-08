e370e1c6caaa1220531c4b269222705e345ceb4e
Marianna Khanina <marianna_khanina@epam.com>
2021-10-08 15:13:03 +0300

    Added Errors Panel Component.

--- src/modules/errors/components/ErrorsPanel.vue
+++ src/modules/errors/components/ErrorsPanel.vue
@@ -25,12 +25,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   // setup () {
   // },
-})
+});
 </script>
 
 <style lang="scss">
