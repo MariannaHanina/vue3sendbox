032c92756c9169e7b335bfa39146f098a0c51974
Marianna Khanina <marianna_khanina@epam.com>
2021-10-12 16:31:51 +0300

    Deleted mock data from api file

--- src/modules/acl/api.ts
+++ src/modules/acl/api.ts
@@ -5,16 +5,4 @@ const apiHttp = ApiHttpSingleton.getInstance();
 
 export async function fetchACLs (): Promise<TAcl[]> {
   return await apiHttp.get('/acls');
-  // const acls: TAcl[] = [{
-  //   id: 'id1',
-  //   name: 'Name1',
-  //   principal: 'principal1',
-  //   resourceType: 'resourceType1',
-  //   patternType: 'patternType1',
-  //   operation: 'operation1',
-  //   host: 'host1',
-  //   permissionType: 'permissionType1',
-  // }];
-
-  // return Promise.resolve(acls);
 }
